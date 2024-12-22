89cce7230cf512dc0e43bebe278a2fb57f5b3b6f
Yaroslav Proidysvit <gona300000@gmail.com>
2024-12-22 16:16:36 +0200

    dz complete

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,5 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useProductById } from "../../hooks/useProductById"
+import "./ProductPage.css"
 
 export function ProductPage (){
     const params = useParams()
@@ -7,12 +8,23 @@ export function ProductPage (){
     const {product} = useProductById(Number(params.id))
 
     return (
-        <div className="product">
-            <h1>Product: {product?.title}</h1>
-            <p>Description:{product?.description}</p>
-            <img src={product?.image} alt="img" />
-            <p>Price: {product?.price}</p>
-            <p>Category: {product?.category}</p>
+        <div className="ProductPage-div">
+            <span className="ProductPage-span">
+                <div className="ProductPage-image-div">
+                    <img className="ProductPage-image" src={product?.image} alt="img" />
+                </div>
+                <div className="Productpage-content">
+                    <h1>Product: {product?.title}</h1>
+                    <p>Description:{product?.description}</p>
+                    <p>Price: {product?.price}</p>
+                    <p>Category: {product?.category}</p>
+                    <span className="span1">
+                        <button className="button-card">В корзину</button>
+                        <button className="button-buy">Купить</button>
+                    </span>
+                </div>
+                
+            </span>
             {/* <h6>{params.id}</h6>    */}
         </div>
     )
