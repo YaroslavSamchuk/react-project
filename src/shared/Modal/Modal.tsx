b346dbd3544ec702aa446163dd38b691fb48a2fd
Yaroslav <fil.i.al4ik@gmail.com>
2025-01-14 17:59:57 +0100

    add comment

--- src/shared/Modal/Modal.tsx
+++ src/shared/Modal/Modal.tsx
@@ -1,23 +1,26 @@
 import { ReactNode, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
-
+//создаем интерфейс , пропс 
 interface IModalProps {
+	//задаем в интервейсе что он будет "дочерним"
 	children: ReactNode;
+	//создаем переменную? состояние тру фолз для состояния модального окна
 	doCloseOutside: boolean;
 	onClose: () => void;
 	container?: Element;
 }
-
+//
 export function Modal(props: IModalProps) {
+	//деструктуризируем пропс
 	const {
 		children,
 		doCloseOutside,
 		onClose,
 		container = document.body,
 	} = props;
-
+//создаем хук useRef для модалки
 	const modalRef = useRef<HTMLDivElement | null>(null);
-
+	//функция закрытия окна при нажатии за модалкой
 	function handleClickOutside(event: MouseEvent) {
 		if (event.target != modalRef.current) {
 			onClose();
@@ -32,6 +35,7 @@ export function Modal(props: IModalProps) {
 			};
 		}
 	}, []);
+	//портал позволяюший возвысить див в иерархии что бы он был поверх остальных елементов
 	return createPortal(
 		<div ref={modalRef} className="modal">
 			{children}
--- src/shared/SearchBar/SearchBar.tsx
+++ src/shared/SearchBar/SearchBar.tsx
@@ -1,79 +1,14 @@
 
-import "./SearchBar.css"
-import { useState, useRef, useEffect } from "react"
-//функция нашего поисковика которая сразу экспортируется
-export function SearchBar() {
-  //записываем состояние модального окна , открыто ли оно
-    const [isModalOpen, setIsModalOpen] = useState(false)
-  //записываем в переменную div елемент , оно будет работать по принципу id что бы divы различались но не используя id 
-  const modalRef = useRef<HTMLDivElement| null>(null)
-  // то же самое для инпутов
-  const inputRef = useRef<HTMLInputElement| null>(null)
-  // проверка на фокус инпута , или же условно кликнут ли инпут
-    const inputOnFocus = () => {
-    // если открыт то закрыть
-        if(isModalOpen) {
-            setIsModalOpen(false)
-      // если закрыт то отображать
-        } else {
-            setIsModalOpen(true)
-        }
-    }
-  // Используем хук useEffect для отслеживания кликов вне модального окна// Функция для обработки кликов вне модального окна
-  useEffect(()=>{
-    // Функция для обработки кликов вне модального окна
-    function handleClickOutside(event: MouseEvent){
-      //Если клик был не на модальном окне и не на инпуте то закрыть 
-        if(event.target != modalRef.current && inputRef.current != event.target){
-          setIsModalOpen(false)
-          
-      }
-    }
-    //Добавляем обработчик события клика
-    document.addEventListener("click", handleClickOutside )
-    //Очищаем обработчик события при размонтировании компонента
-    return()=>{
-      document.removeEventListener("click", handleClickOutside)
-    }
-  },[])
-
-  return (
-    //
-    <div className="searchBar">
-      {/*  Поле ввода с обработчиком фокуса*/}
-      <input type="text" onFocus={inputOnFocus} ref={inputRef} placeholder="Пошук продукта" />
-      {/* иконка */}
-      <svg
-        width="28"
-                className="search-icon"
-        height="28"
-        viewBox="0 0 28 28"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M26 26L20.2 20.2M23.3333 12.6667C23.3333 18.5577 18.5577 23.3333 12.6667 23.3333C6.77563 23.3333 2 18.5577 2 12.6667C2 6.77563 6.77563 2 12.6667 2C18.5577 2 23.3333 6.77563 23.3333 12.6667Z"
-          stroke="#1E1E1E"
-          stroke-width="4"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </svg>
-      {/* Модальное окно, которое открывается при фокусе на инпуте */}
-            {isModalOpen ? (
-                <div ref={modalRef}>Opened</div>
-            ) : null}
-    </div>
-  );
-}
-
 import { Modal } from "../Modal/Modal";
 import "./SearchBar.css";
 import { useState, useRef, useEffect } from "react";
-
+//функция нашего поисковика которая сразу экспортируется
 export function SearchBar() {
+  //записываем состояние модального окна , открыто ли оно
 	const [isModalOpen, setIsModalOpen] = useState(false);
+   //записываем в переменную div елемент , оно будет работать по принципу id что бы divы различались но не используя id 
 	const modalContainerRef = useRef<HTMLDivElement | null>(null);
+   // проверка на фокус инпута , или же условно кликнут ли инпут
 	const inputOnFocus = () => {
 		if (isModalOpen) {
 			setIsModalOpen(false);
@@ -82,6 +17,7 @@ export function SearchBar() {
 		}
 	};
 	return (
+    // div с модалкой
 		<div
 			className="searchBar"
 			ref={modalContainerRef}
@@ -110,6 +46,7 @@ export function SearchBar() {
 					stroke-linejoin="round"
 				/>
 			</svg>
+      {/* сама модалка и проверка ее */}
 			{isModalOpen ? (
 				<Modal
 					doCloseOutside={false}
